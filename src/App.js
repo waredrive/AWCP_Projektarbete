a8a7e71d79d935f24fdb20172cc8965c48ce60cb
Lukas S <172839@protonmail.com>
2018-11-05 11:03:35 +0100

    minor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import axios from './axios-settings';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  testApiCall = () => 'hello';
+
   render() {
-    return <div>Hello</div>;
+    return <div>{testApiCall}</div>;
   }
 }
 
--- src/axios-settings.js
+++ src/axios-settings.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://react-my-burger-4685f.firebaseio.com/'
+  baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default instance;
