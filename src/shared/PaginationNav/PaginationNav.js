04a77e7f0cd951d2eec2e79a26180cb392f13915
Lukas S <172839@protonmail.com>
2018-11-20 17:38:58 +0100

    commenting

--- src/shared/PaginationNav/PaginationNav.js
+++ src/shared/PaginationNav/PaginationNav.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
+// This class creates pagination bar that consist of ten pages, a previous and a next button.
+// If a user is att the end of the bar and clicks the next button the next ten pages will be
+// shown upp to the total amount of pages. Same if the pervious button is clicked at the start
+// of the bar where the page nr 1 is the limit.
 class PaginationNav extends Component {
   changePage = changedPage => {
     const { onPageChanged, currentPage, totalPages } = this.props;
@@ -18,6 +22,8 @@ class PaginationNav extends Component {
     const { currentPage, totalPages } = this.props;
     const pagination = [];
 
+    // Calculates the start point of the bar based on current page.
+    //
     const startPoint =
       Math.floor(currentPage / 10) * 10 === 0
         ? 1
@@ -26,6 +32,7 @@ class PaginationNav extends Component {
     const endPoint =
       startPoint + 10 > totalPages ? totalPages + 1 : startPoint + 10;
 
+    // Creates pagination-buttons based on startPoint and endPoint calculated above.
     for (let i = startPoint; i < endPoint; i += 1) {
       pagination.push(
         <PaginationItem key={i} active={currentPage === i} id={i}>
